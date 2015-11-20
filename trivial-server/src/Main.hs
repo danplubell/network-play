9a35121a92c32edfe076732d20f1226cf9ebe635
Dan Plubell <dplubell@cerner.com>
2015-11-20 15:00:06 -0600

    add channel communication to trivial server

--- trivial-server/src/Main.hs
+++ trivial-server/src/Main.hs
@@ -1,9 +1,19 @@
 module Main where
 {-Simple version that uses System.IO-}
 {-Add concurrency to main loop-}
+{-Add comminication between threads-}
+{-This version has a memory leak because the originla thread is not read
+  Closing connection isn't handled well
+  Need to add exception handling
+-}
 import           Control.Concurrent
+import           Control.Monad
+import           Control.Monad.Fix  (fix)
 import           Network.Socket
 import           System.IO
+
+type Msg = String
+
 main :: IO ()
 main = do
   --create a socket
@@ -18,18 +28,31 @@ main = do
   -- allow as maximum of 2 outstanding connections
   listen sock 2
 
+  --create a channel
+  chan <- newChan
   -- start main processing loop
-  mainLoop sock
+  mainLoop sock chan
 
-
-mainLoop sock = do
+mainLoop::Socket -> Chan Msg -> IO ()
+mainLoop sock chan = do
     conn <- accept sock
-    _ <- forkIO (runConn sock)
-    mainLoop sock
-    undefined
-runConn:: (Socket,SockAddr)->IO ()
-runConn (sock, _) = do
+    _ <- forkIO (runConn conn chan)
+    mainLoop sock chan
+
+-- runConn duplicates the channel and reads from it
+runConn:: (Socket,SockAddr)->Chan Msg ->IO ()
+runConn (sock, _) chan = do
+            let broadcast  = writeChan chan
             hdl <- socketToHandle sock ReadWriteMode
             hSetBuffering hdl NoBuffering
-            hPutStrLn hdl "Hi!"
-            hClose hdl
+            chan' <- dupChan chan
+            --fork off thread for reading from duplicated channel
+            _ <- forkIO $ fix $ \loop -> do
+                line <- readChan chan'
+                hPutStrLn hdl line
+                loop
+            fix $ \loop -> do
+                line <- liftM init (hGetLine hdl)
+                broadcast line
+                loop
+
